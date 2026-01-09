d4f9c5f0e4219e0cae0732bcdefa18d8b6f94ec0
Kalpani Ekanayake <ekanayake.emkn@gmail.com>
2026-01-09 23:08:42 +0530

    Add sementic html login form

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <title>login</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+
+    <form method ="post>
+    <label>Email</label>
+    <input type ="email" name="email required>
+
+    <label>Password</label>
+    <input type="passsword" name="password" required>
+
+    <button type ="submit> Login </button>
+    </form>
+</body>
+</html>
