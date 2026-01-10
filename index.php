44c155fbc018673df112cd7d2a917e37710ac243
Kalpani Ekanayake <ekanayake.emkn@gmail.com>
2026-01-10 12:38:12 +0530

    Add basic dashboard page layout

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "Login System Home";
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Dashboard</title>
+    </head>
+
+    <body>
+        <h1>Dashboard</h1>
+        <p>Welcome! You are logged in.</p>
+ 
+        <a href="login.php">Logout</a>
+    </body>
+</html>
