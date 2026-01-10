3c8822d8f8e3a0beb3fb4d8b0217b8689c151ace
Kalpani Ekanayake <ekanayake.emkn@gmail.com>
2026-01-10 12:32:34 +0530

    Add html registration form layout

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 
-<html>
+<html lang ="en">
     <head>
+    <meta charset="utf-8">
     <title>login</title>
 </head>
 
@@ -9,13 +10,27 @@
     <h1>Login</h1>
 
     <form method ="post>
-    <label>Email</label>
-    <input type ="email" name="email required>
 
-    <label>Password</label>
-    <input type="passsword" name="password" required>
+        <div>
+            <label for ="name" > Full name</label><br>
+            <input type="text" id="name" name ="name" required>
+        </div>
+        <div>
+            <label for ="email">Email</label><br>
+            <input type ="email" name="email required>
+        </div>
 
-    <button type ="submit> Login </button>
+        <div>
+            <label for="password">Password</label><br>
+            <input type="passsword" name="password" required>
+        </div>
+
+        <button type="submit"> Registeer </button>
     </form>
+
+    <p> Already have an account?$_COOKIE<a href="login.php">Login</a>
+</p>
+
+
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF_8">
+        <title>Register</title>
+    </head>
+
+    <body>
+        <h1>Register</h1>
+
+        <form method="post">
+            <div>
+                <label for ="name"> Name</label><br>
+                <input type="text" id="name" name="name" required>
+            </div>
+
+            <div>
+            <label for ="email">Email</label><br>
+            <input type ="email" name="email required>
+        </div>
+
+        <div>
+            <label for="password">Password</label><br>
+            <input type="passsword" name="password" required>
+        </div>
+
+        <button type="submit"> Registeer </button>
+        </form>
+
+    <p> Already have an account?$_COOKIE<a href="login.php">Login</a>
+</p>
+
+
+</body>
+</html>
+        </form>
+    </body>
+</html>
